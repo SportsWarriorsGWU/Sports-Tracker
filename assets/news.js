d189b7cdbec1d88c87a8a00613d1744fa86fc066
Mohammad Chowdhury <aktherjaved@yahoo.com>
2020-01-13 18:25:35 -0500

    Avoid empty image on news page

--- assets/news.js
+++ assets/news.js
@@ -15,29 +15,30 @@
 
                 storyRow = $("<li>")
                 storyRow.attr("class", "media")
-
-                thumbNail = $("<img>").attr("src", response.articles[i].urlToImage);
-                thumbNail.attr("class", "mr-3");
-         
-
-                mediaBody = $("<div>")
-                mediaBody.attr("class", "media-body");
-
-                title = $("<h5>").text(response.articles[i].title);
-                title.attr("class", "media-body", "mt-0", "mb-3", "font-weight-bold");
+                if (response.articles[i].urlToImage !== null) {
+                    thumbNail = $("<img>").attr("src", response.articles[i].urlToImage);
+                    thumbNail.attr("class", "mr-3");
 
 
-                description = $("<p>").text(response.articles[i].description);
-                description.attr("class", "media-body", "mt-3", "mb-1");
+                    mediaBody = $("<div>")
+                    mediaBody.attr("class", "media-body");
 
-                newsLink = $("<a>").text(response.articles[i].url);
-                newsLink.attr("href", response.articles[i].url);
-                newsLink.attr("class", "media-body btn btn-dark", "mt-1", "mb-1").text('Read/Watch News');
+                    title = $("<h5>").text(response.articles[i].title);
+                    title.attr("class", "media-body", "mt-0", "mb-3", "font-weight-bold");
 
-                $(mediaBody).append(title, description, newsLink);
-                $(storyRow).append(thumbNail, mediaBody);
-                $(".news-list").append(storyRow);
 
+                    description = $("<p>").text(response.articles[i].description);
+                    description.attr("class", "media-body", "mt-3", "mb-1");
+
+                    newsLink = $("<a>").text(response.articles[i].url);
+                    newsLink.attr("href", response.articles[i].url);
+                    newsLink.attr("class", "media-body btn btn-dark", "mt-1", "mb-1").text('Read/Watch News');
+
+                    $(mediaBody).append(title, description, newsLink);
+                    $(storyRow).append(thumbNail, mediaBody);
+                    $(".news-list").append(storyRow);
+
+                }
             }
         });
         $('.carousel').carousel({
@@ -48,12 +49,12 @@
             leagueID = $(this)[0].id;
             console.log(leagueID);
         });
-    
+
         $('.tile-images').mouseout(function() {
             leagueID = '';
             console.log(leagueID);
         });
-    
+
         $(window).on('unload', function() {
             if (leagueID !== undefined && leagueID !== '') {
                 localStorage.setItem('newIDToLoad', JSON.stringify(leagueID));
